1a66a269452942cfaffeba71caa6dac43a549de6
oliviervgit <olivier.verhaeghe@galloo.com>
2025-05-07 20:43:12 +0200

    Update Program.cs Olivier

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Cloud Ev");
+app.MapGet("/", () => "Hello World! Cloud Ev Olivier");
 
 app.Run();
